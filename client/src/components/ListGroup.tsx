5cfe7b3bf593ae20b267c38fb1d28dfe5f593765
Adam Tanweer <113802807+Poltanek@users.noreply.github.com>
2025-01-24 07:48:03 +0000

    update

---
+++ client/src/components/ListGroup.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+function ListGroup() {
+    const items = [
+        'New York',
+        'San Francisco',
+        'Tokyo',
+        'London',
+        'Paris'
+    ];
+    const [selectedIndex, setSelectedIndex] = useState(-1);
+
+    const getMessage = () => {
+        items.length === 0 ? <p>No item found</p> : null;
+    }
+
+
+    return (
+        <>
+            <h1>List</h1>
+            {getMessage()}
+            <ul className="list-group">
+                {items.map((item, index) => (
+                    <li
+                        className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'}
+                        key={item}
+                        onClick={() => {setSelectedIndex(index); }}
+                    >
+                        {item}
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
+}
+
+export default ListGroup
